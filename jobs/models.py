7c2d894034d9fda129bbc803db989f2c9263bd69
yazan <yazan.ayoub.bee@outlook.com>
2021-12-25 00:18:46 +0100

    finish the basic function

--- jobs/models.py
+++ jobs/models.py
@@ -1,8 +1,9 @@
 from django.db import models
-
 # Create your models here.
 class Job(models.Model):
     image = models.ImageField(upload_to='images/')
     Titel=models.TextField(max_length=100)
     summary = models.CharField(max_length=500)
 
+    def __str__(self):
+        return self.Titel
