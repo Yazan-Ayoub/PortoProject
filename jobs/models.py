f37aa342522bfc81d5a9c3c0b77ea8c80b1f90a0
yazan <yazan.ayoub.bee@outlook.com>
2021-12-24 22:46:57 +0100

    add models

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Job(models.Model):
+    image = models.ImageField(upload_to='images/')
+    Titel=models.TextField(max_length=100)
+    summary = models.CharField(max_length=500)
+
